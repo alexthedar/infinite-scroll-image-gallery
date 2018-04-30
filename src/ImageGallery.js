24fba9fc1bc3611d774cf7382cb58bbd92806a71
Alexandar Castaneda <acastane@partner.jaguarlandrover.com>
2018-04-30 11:39:17 -0700

    variable number array based on screen width

--- src/ImageGallery.js
+++ src/ImageGallery.js
@@ -13,11 +13,49 @@ const getNumberArray = (starting, amount) => {
 
 class ImageGallery extends Component {
 
-  state={}
+  state={
+    currentVisibleStart: 1500348260,
+    currentVisibleEnd: 1500348260,
+    currentDisplayStart: 1500348260,
+    currentDisplayEnd: 1500348260,
+    windowWidth: 0,
+    windowHeight: 0
+  }
+
+  componentDidMount() {
+    this.updateWindowDimensions();
+    window.addEventListener('resize', this.updateWindowDimensions);
+    window.addEventListener('scroll', this.handleScroll);
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener('resize', this.updateWindowDimensions);
+    window.removeEventListener('scroll', this.handleScroll);
+  }
+
+  updateWindowDimensions = (e) =>  {
+    this.setState({ windowWidth: window.innerWidth, windowHeight: window.innerHeight });
+    // console.log('window resize');
+    // console.log(e);
+
+  }
+
+  handleScroll = (e) => {
+    // console.log('scroll event');
+    // console.log(e);
+  }
 
   render() {
-    let arr = getNumberArray(1500348260, 40)
-    
+    console.log(this.state.windowWidth)
+    let amount = 18
+    if(this.state.windowWidth < 400) { amount = 16 }
+    else if (this.state.windowWidth >= 400 && this.state.windowWidth < 768) { amount = 8 }
+    else if (this.state.windowWidth >= 768 && this.state.windowWidth < 992) { amount = 18 }
+    else if (this.state.windowWidth >= 992 && this.state.windowWidth < 1200) { amount = 24 }
+    else if (this.state.windowWidth >= 1200 ) { amount = 21 }
+    console.log(amount)
+    let arr = getNumberArray(1500348260, amount)
+    // 18 24 16 16  
     let photos = arr.map((number, i) => {
       return <Photo url={number} key={number+`${i}`}/>
     })
