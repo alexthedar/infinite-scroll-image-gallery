2c2d4cc211db249ad2a1f948f6078e923176d919
alexthedar <alex.castaneda@gmail.com>
2018-05-01 19:29:37 -0700

    rows per page working need to translate that into photo numbers or use as idx on array

--- src/ImageGallery.js
+++ src/ImageGallery.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
+import Measure from 'react-measure';
 import { Grid, Row, Col, Image } from 'react-bootstrap';
 import './ImageGallery.css'
 
@@ -38,7 +39,7 @@ class ImageGallery extends Component {
 
   componentWillMount(){
     let amount = getRecordAmount(window.innerWidth);
-    let photoArr = getNumberArray(1500348260, amount);
+    let photoArr = getNumberArray(1500348260, 100);
     this.setState({
       windowWidth: window.innerWidth,
       windowHeight: window.innerHeight,
@@ -98,19 +99,24 @@ class ImageGallery extends Component {
     // let N = 2
     // N += absY > 1000 ? Math.floor(window.innerHeight/1.2) : Math.floor(absY/250)
     // console.log(absY, offset, N)
-    console.log(this.refs.scrollThing.findDOMNode())
-    // var visibleStart = Math.floor(scroll / this.state.recordHeight);
-    // var visibleEnd = Math.min(visibleStart + this.state.recordsPerBody, this.state.total - 1);
+    // console.log(this.refs.scrollThing.findDOMNode())
+    console.log(this.state)
+    var scrollTop = document.documentElement.scrollTop || window.pageYOffset;
+    var recordPerBody = Math.floor((window.innerHeight - 2) / this.state.photoHeight);
+    var visibleStart = Math.floor(scrollTop / this.state.photoHeight);
+    var visibleEnd = Math.min(visibleStart + recordPerBody, 500000 - 1);
 
-    // var displayStart = Math.max(0, Math.floor(scroll / this.state.recordHeight) - this.state.recordsPerBody * 1.5);
-    // var displayEnd = Math.min(displayStart + 4 * this.state.recordsPerBody, this.state.total - 1);
+    var displayStart = Math.max(0, Math.floor(scrollTop / this.state.photoHeight) - recordPerBody * 1.5);
+    var displayEnd = Math.min(displayStart + 4 * recordPerBody, 500000 - 1);
+
+    console.log(recordPerBody, visibleStart, visibleEnd, displayStart, displayEnd) //window.pageYOffset || 
     if(window.scrollY < 300 && document.body.offsetHeight > 4000){
       console.log('here')
     }
     if ( (window.innerHeight + window.scrollY) >= (document.body.offsetHeight - 250) ) {
       let endingNumber = this.state.photosArr[this.state.photosArr.length-1];
       this.setState({loading: true})
-      this.addToPhotos(endingNumber, this.state.amount)
+      // this.addToPhotos(endingNumber, this.state.amount)
       // if(this.state.photosArr.length > 200){
       //   this.updateArray(endingNumber)
       // } else {
@@ -118,10 +124,16 @@ class ImageGallery extends Component {
     }
   }
 
+  getPhotoHeight = (height) => {
+    this.setState({
+      photoHeight: height
+    })
+  }
+
   render() {
-    // console.log(this.state.currentDisplayEnd)
+    // console.log(this.state)
     let photos = this.state.photosArr.map((number, i) => {
-      return <Photo url={number} key={number+`${i}`}/>
+      return <Photo url={number} key={number+`${i}`} getPhotoHeight={this.getPhotoHeight}/>
     })
 
     return (
@@ -139,10 +151,34 @@ class ImageGallery extends Component {
 export default ImageGallery;
 
 
-const Photo = (props) => {
-  return (
-    <Col xs={12} sm={6} md={4} lg={4} > 
-      <Image src={`https://hiring.verkada.com/thumbs/${props.url}.jpg`} responsive className='photo-box'/>
-    </Col>     
-  )
+
+class Photo extends Component {
+
+  render (){
+    return(
+      <Measure
+        bounds
+        onResize={(contentRect) => {
+          this.props.getPhotoHeight(contentRect.bounds.height)
+        }}>
+        {({ measureRef }) =>
+            <Col xs={4} sm={4} md={4} lg={4} > 
+              <div ref={measureRef}>
+                <Image src={`https://hiring.verkada.com/thumbs/${this.props.url}.jpg`} responsive className='photo-box'/>
+              </div>
+            </Col>     
+        }
+      </Measure>    )
+  }
 }
+
+
+// const Photo = (props) => {
+//   return (
+//     <Col xs={12} sm={6} md={4} lg={4} > 
+//       <Image src={`https://hiring.verkada.com/thumbs/${props.url}.jpg`} responsive className='photo-box'/>
+//     </Col>     
+//   )
+// }
+
+
