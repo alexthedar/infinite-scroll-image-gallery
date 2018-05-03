d09f21836afddb0daf8c997dab25c83739608d87
Alexandar Castaneda <acastane@partner.jaguarlandrover.com>
2018-05-03 09:39:28 -0700

    need loading so it can refresh arr

--- src/ImageGallery.js
+++ src/ImageGallery.js
@@ -25,39 +25,35 @@ const getRecordAmount = (windowWidth) => {
   return amount;
 }
 
+const getPhotoNumbersArr = (start, end) => {
+  let arr = []
+  for(let i = start; i <= end; i=i+20){
+    arr.push(i)
+  }
+  return arr
+}
+
 class ImageGallery extends Component {
 
   state={
-    windowWidth: 0,
-    windowHeight: 0,
+    windowWidth: window.innerWidth,
+    windowHeight: window.innerHeight,
     loading: false,
-    totalPhotos: (1503031520-1500348260)/5 // start hundreds - end hundreds / photos per hundred
-  }
-
-  componentWillMount(){
-    let amount = getRecordAmount(window.innerWidth);
-    let photosArr = getNumberArray(1500348260, 50);
-    let rowsPerPage = Math.floor((window.innerHeight - 2) / 220); //default size
-    let displayEndRow = rowsPerPage * 2; //default size
-    this.setState({
-      windowWidth: window.innerWidth,
-      windowHeight: window.innerHeight,
-      amount: amount,
-
-      rowsPerPage: rowsPerPage,
-      columnsPerPage: 3,
-      photosPerPage: rowsPerPage * 3,
-      visibleStartRow: 1,
-      visibleEndRow: rowsPerPage * 2,
-      displayStartRow: 1,
-      displayEndRow: rowsPerPage * 2,
-      firstVisiblePhoto: 1500348260,
-      lastVisiblePhoto: 1500348240 + ((rowsPerPage * 3) * 20),
-      firstDisplayPhoto: 1500348260,
-      lastDisplayPhoto: 1500348240 + ((displayEndRow * 3) *20),
-      photoHeight: 500
-    })
-  }
+    totalPhotos: (1503031520-1500348260)/5, // start hundreds - end hundreds / photos per hundred
+    rowsPerPage: 3,
+    columnsPerPage: 3,
+    photosPerPage: 9,
+    visibleStartRow: 0,
+    visibleEndRow: 3,
+    displayStartRow: 0,
+    displayEndRow: 6,
+    firstVisiblePhoto: 1500348260,
+    lastVisiblePhoto: 1500348300,
+    firstDisplayPhoto: 1500348260,
+    lastDisplayPhoto: 1500348420,
+    photoHeight: 250,
+    photosNumbers: getPhotoNumbersArr(1500348260, 1500348420)
+}
 
   componentDidMount() {
     this.updateWindowDimensions();
@@ -74,35 +70,44 @@ class ImageGallery extends Component {
     let amount = getRecordAmount(window.innerWidth);
     // let calcs = this.getPhotoCalc();
     let calcs = {}
-    calcs.windowWidth = window.innerWidth;
-    calcs.windowHeight = window.innerHeight;
+    calcs.windowWidth = document.documentElement.clientWidth;
+    calcs.windowHeight = document.documentElement.clientHeight;
     calcs.amount = amount;
     this.setState(calcs);
   }
   
   handleScroll = (e) => {
-    let data = this.getPhotoCalc();
-    this.setState(data)
+    this.getPhotoCalc();
+    // this.setState(...data)
   }
 
   getPhotoCalc = () => {  
-    let rowsPerPage = Math.floor((window.innerWidth - 2) / this.state.photoHeight);
-    let columnsPerPage = Math.floor(window.innerWidth / this.state.photoWidth);
+    let rowsPerPage = Math.floor((document.documentElement.clientHeight - 2) / this.state.photoHeight);
+    let columnsPerPage = Math.floor(document.documentElement.clientWidth / this.state.photoWidth);
 
     let scrollTop = document.documentElement.scrollTop || window.pageYOffset;
     let photosPerPage = rowsPerPage * columnsPerPage;
 
     let visibleStartRow = Math.floor(scrollTop / this.state.photoHeight);
     let visibleEndRow = Math.min(visibleStartRow + rowsPerPage, 500000 - 1);
-    let displayStartRow = Math.max(0, Math.floor(scrollTop / this.state.photoHeight) - rowsPerPage * 1.5);
-    let displayEndRow = Math.min(displayStartRow + 4 * rowsPerPage, 500000 - 1);
+    let displayStartRow = Math.max(0, Math.floor((scrollTop / this.state.photoHeight) - rowsPerPage * 1.5));
+    let displayEndRow = Math.min(displayStartRow + (4 * rowsPerPage), 500000 - 1);
 
     let firstVisiblePhoto = 1500348260 + ((visibleStartRow * columnsPerPage)*20);
     let lastVisiblePhoto = (firstVisiblePhoto - 20) + ((rowsPerPage * columnsPerPage) * 20);
     let firstDisplayPhoto = 1500348260 + ((displayStartRow * columnsPerPage) * 20);
     let lastDisplayPhoto = (firstVisiblePhoto - 20) + ((displayEndRow * columnsPerPage) *20);
+    if(photosPerPage > 40){
+      console.log(this.state)
+      debugger;
+    }
+    console.log(this.state)
+    console.log(rowsPerPage, columnsPerPage, scrollTop, photosPerPage, visibleStartRow, visibleEndRow, displayStartRow, displayEndRow, firstVisiblePhoto, lastVisiblePhoto, firstDisplayPhoto, lastDisplayPhoto)
 
-    return {
+    let photosNumbers = getPhotoNumbersArr(this.state.firstDisplayPhoto, this.state.lastDisplayPhoto)
+
+    console.log(photosNumbers)
+    this.setState({
       rowsPerPage: rowsPerPage,
       columnsPerPage: columnsPerPage,
       photosPerPage: photosPerPage,
@@ -113,28 +118,27 @@ class ImageGallery extends Component {
       firstVisiblePhoto: firstVisiblePhoto,
       lastVisiblePhoto: lastVisiblePhoto,
       firstDisplayPhoto: firstDisplayPhoto,
-      lastDisplayPhoto: lastDisplayPhoto
-    }
+      lastDisplayPhoto: lastDisplayPhoto,
+      photosNumbers: photosNumbers
+    })
   }
 
   getPhotoDimensions = (height, width) => {
     this.setState({
       photoHeight: height,
-      photoWidth: width,
-      columnsPerPage: Math.floor(window.innerWidth / width),
+      photoWidth: width
     })
   }
 
 
   render() {
 
-    // console.log(this.state)
     // var photos = []
     // for(let i = this.state.firstVisiblePhoto; i< this.state.lastDisplayPhoto; i=i+20){
     //   photos.push(<Photo url={i} key={i} getPhotoDimensions={this.getPhotoDimensions}/>)
     // }
 
-    // let photos = this.state.photosArr.map((number, i) => {
+    // let photos = this.state.photosNumbers.map((number, i) => {
     //   return <Photo url={number} key={number+`${i}`} getPhotoDimensions={this.getPhotoDimensions}/>
     // })
     // console.log(photos)
@@ -143,16 +147,19 @@ class ImageGallery extends Component {
       <React.Fragment >
         <Grid>
           <PhotoRows  getPhotoDimensions={this.getPhotoDimensions} 
-                      firstVisiblePhoto={this.state.firstVisiblePhoto}
-                      lastVisiblePhoto={this.state.lastVisiblePhoto}
-                      firstDisplayPhoto={this.state.firstDisplayPhoto}
-                      lastDisplayPhoto={this.state.lastDisplayPhoto}
-                      photoHeight={this.state.photoHeight}
-                      visibleStartRow={this.state.visibleStartRow}
-                      visibleEndRow={this.state.visibleEndRow}
-                      displayStartRow={this.state.displayStartRow}
-                      displayEndRow={this.state.displayEndRow}
-                      total={this.state.totalPhotos}/>
+            firstVisiblePhoto={this.state.firstVisiblePhoto}
+            lastVisiblePhoto={this.state.lastVisiblePhoto}
+            firstDisplayPhoto={this.state.firstDisplayPhoto}
+            lastDisplayPhoto={this.state.lastDisplayPhoto}
+            photoHeight={this.state.photoHeight}
+            visibleStartRow={this.state.visibleStartRow}
+            visibleEndRow={this.state.visibleEndRow}
+            displayStartRow={this.state.displayStartRow}
+            displayEndRow={this.state.displayEndRow}
+            total={this.state.totalPhotos}
+            photosNumbers={this.state.photosNumbers}/>
+
+
         </Grid>
       </React.Fragment>
     );
@@ -160,6 +167,13 @@ class ImageGallery extends Component {
 }
 
 export default ImageGallery;
+
+
+        // <Row style={{height: this.state.displayStartRow * this.state.photoHeight}}></Row>
+        // <Row>
+        //   {photos}
+        // </Row>
+        // <Row style={{height: (this.state.displayEndRow *5) * this.state.photoHeight}}></Row>
 
 
 class PhotoRows extends Component {
@@ -175,18 +189,15 @@ class PhotoRows extends Component {
     var shouldUpdate = !(
         nextProps.visibleStartRow >= this.state.displayStartRow &&
         nextProps.visibleEndRow <= this.state.displayEndRow
-    ) || (nextProps.total !== this.state.total);
-
+    );
+    console.log(shouldUpdate)
     if (shouldUpdate) {
         this.setState({
             shouldUpdate: shouldUpdate,
-            total: nextProps.total,
-            displayStartRow: nextProps.displayStartRow,
-            displayEndRow: nextProps.displayEndRow,
-            photoVisibleStart: nextProps.firstVisiblePhoto,
-            photoVisibleEnd: nextProps.lastVisiblePhoto,
-            photoDisplayStart: nextProps.firstDisplayPhoto,
-            photoDisplayEnd: nextProps.lastDisplayPhoto
+            visibleStartRow: nextProps.firstVisiblePhoto,
+            visibleEndRow: nextProps.lastVisiblePhoto,
+            displayStartRow: nextProps.firstDisplayPhoto,
+            displayEndRow: nextProps.lastDisplayPhoto
 
         });
     } else {
@@ -194,30 +205,26 @@ class PhotoRows extends Component {
     }
   }
 
-  shouldComponentUpdate(){
-    return this.state.shouldUpdate
-  }
+  // shouldComponentUpdate(){
+  //   return this.state.shouldUpdate
+  // }
 
 
   render(){
     console.log(this.props)
-    var photos = []
-    for(let i = this.props.firstVisiblePhoto; i< this.props.lastDisplayPhoto; i=i+20){
-      // photos.push(<Photo url={i} key={i} getPhotoDimensions={this.props.getPhotoDimensions}/>)
-      console.log(i)
-      photos = photos.concat([<Photo url={i} key={i} getPhotoDimensions={this.props.getPhotoDimensions}/>])
-    }
+    let photos = this.props.photosNumbers.map((number, i) => {
+      return <Photo url={number} key={number+`${i}`} getPhotoDimensions={this.props.getPhotoDimensions}/>
+    })
 
 
     return(
       <React.Fragment>
         <Row style={{height: this.props.displayStartRow * this.props.photoHeight}}></Row>
-        <Row>
-          {photos}
-        </Row>
+          <Row>
+            {photos}
+          </Row>
         <Row style={{height: (this.props.displayEndRow *5) * this.props.photoHeight}}></Row>
       </React.Fragment>
-
     )
   }
 }
@@ -226,21 +233,18 @@ class PhotoRows extends Component {
 
 class Photo extends Component {
 
-  componentWillMount(){
-
-  }
-
   render (){
     return(
       <Measure
         bounds
         onResize={(contentRect) => {
           this.props.getPhotoDimensions(contentRect.bounds.height, contentRect.bounds.width)
+          console.log(contentRect.bounds.height)
         }}>
         {({ measureRef }) =>
             <Col xs={12} sm={6} md={4} lg={4} > 
               <div ref={measureRef} >
-                <Image src={`https://hiring.verkada.com/thumbs/${this.props.url}.jpg`} responsive className='photo-box'/>
+                <Image src={`https://hiring.verkada.com/thumbs/${this.props.url}.jpg`} className='photo-box' />
                 <span style={{color: 'white'}}>{this.props.url}</span>
               </div>
             </Col>     
@@ -253,7 +257,7 @@ class Photo extends Component {
 // const Photo = (props) => {
 //   return (
 //     <Col xs={12} sm={6} md={4} lg={4} > 
-//       <Image src={`https://hiring.verkada.com/thumbs/${props.url}.jpg`} responsive className='photo-box'/>
+//       <Image src={`https://hiring.verkada.com/thumbs/${props.url}.jpg`}  className='photo-box' style={{height: '250px'}}/>
 //     </Col>     
 //   )
 // }
