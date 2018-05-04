9c4822f7856060919f0355891d35d3981071e959
Alexandar Castaneda <acastane@partner.jaguarlandrover.com>
2018-05-04 16:01:51 -0700

    close but still not there

--- src/ImageGallery.js
+++ src/ImageGallery.js
@@ -39,11 +39,11 @@ const getColumns = (winWidth, photoWidth) => {
 }
 
 const getRows = (winHeight, photoHeight) => {
-  return Math.max(1, Math.floor(winHeight/photoHeight))
+  return Math.max(1, Math.ceil(winHeight/photoHeight))
 }
 
 const getScrollRow = (scrollTop, photoHeight) => {
-  return Math.floor(scrollTop/photoHeight)
+  return Math.floor(scrollTop/Math.ceil(photoHeight))
 }
 
 const getPhotoNumbersArr = (start, end) => {
@@ -60,12 +60,12 @@ class ImageGallery extends Component {
     windowWidth: window.innerWidth,
     windowHeight: window.innerHeight,
     loading: false,
-    totalPhotos: (1503031520-1500348260)/5, // start hundreds - end hundreds / photos per hundred
+    totalPhotos: 2683260, // start hundreds - end hundreds / photos per hundred
     photosPerPage: 15,
-    photoHeight: (window.innerHeight/getAmount(window.innerWidth).row),
-    elapsedScrollHeight: 0,
-    remainingScrollHeight: (1503031520-150034826)/(window.innerHeight/getAmount(window.innerWidth).row),
-    photosNumbers: getNumberArray(1500348260, 15)  || 0,
+    photoHeight: 290,
+    elapsedScrollHeight: 50,
+    remainingScrollHeight: 9202,
+    photosNumbers: getNumberArray(1500348260, 30)  || 0,
     row: 0
 }
 
@@ -84,6 +84,7 @@ class ImageGallery extends Component {
     let w = window.innerWidth;
     let h = window.innerHeight
     this.setState({
+      // rowsPerPage: getRows(h, this.state.photoHeight),
       rowsPerPage: getRows(h, this.state.photoHeight),
       columnsPerPage: getColumns(w, this.state.photoWidth),
       windowWidth: w,
@@ -93,38 +94,40 @@ class ImageGallery extends Component {
   
   handleScroll = (e) => {
     this.setState({ loading: true })
-    let photo = Math.ceil(this.state.photoHeight)
+    let photo = Math.ceil(this.state.photoHeight);
     let scroll = document.documentElement.scrollTop || window.pageYOffset;
-    console.log(document.documentElement.scrollTop, window.pageYOffset, photo)
-    console.log(Math.floor(document.documentElement.scrollTop/photo), Math.floor(window.pageYOffset/photo))
+    let topVisibleRow = getScrollRow(scroll, this.state.photoHeight);
+    let bottomVisibleRow = topVisibleRow + this.state.rowsPerPage;
+    let firstVisiblePhoto = 1500348260 + (topVisibleRow * 60)
+    let lastVisiblePhoto = (firstVisiblePhoto -20) + (bottomVisibleRow * 60)
+    let topDisplayRow = (topVisibleRow - 3) < 0 ? 0 : (topVisibleRow - 3);
+    let bottomDisplayRow = topDisplayRow + (this.state.rowsPerPage * 4)
+    let topDisplayPhoto = topDisplayRow < 0 ? firstVisiblePhoto : 1500348260 + (topDisplayRow * 60);
+    let bottomDisplayPhoto = (firstVisiblePhoto -20) + (bottomDisplayRow * 60)
 
-    // let arr = this.getPhotoArr();
-    let newArr = this.state.photosNumbers.concat([])
-    // let adjust = scroll < this.state.scroll ? -60 : 60;
-    // let rowAdjust = scroll < this.state.scroll ? -1 : 1;
-    let row = this.state.row + 0;
-    if (row < 0){row = 0}
-    let first = newArr[0]+0;
-    if(first < 1500348260){first = 1500348260}
-    let last = newArr[14]+0;
-    if(last < 1500348500) {last = 1500348500}
-    let remadeArr = getPhotoNumbersArr(1500348260, 1500349500)
-    let totalScrollHeight =((1503031520-1500348260))/this.state.photoHeight;
-    let elapsedScrollHeight = this.state.photoHeight * row;
-    let remainingScrollHeight = totalScrollHeight - (elapsedScrollHeight + (this.state.photoHeight * 4))
+    let totalScrollHeight =9252;
+    let elapsedScrollHeight = this.state.photoHeight * topVisibleRow;
+    let remainingScrollHeight = totalScrollHeight - elapsedScrollHeight
+
+    console.log(topDisplayRow, bottomDisplayRow)
+    console.log(topDisplayPhoto, bottomDisplayPhoto)
     this.setState({
-      photosNumbers: remadeArr,
+      // photosNumbers: remadeArr,
       scroll: scroll,
-      row: row,
+      row: topVisibleRow,
       elapsedScrollHeight: elapsedScrollHeight,
-      remainingScrollHeight: remainingScrollHeight
+      remainingScrollHeight: remainingScrollHeight,
+      topDisplayRow: topDisplayRow,
+      topVisibleRow: topVisibleRow,
+      topDisplayPhoto: topDisplayPhoto,
+      bottomDisplayPhoto: bottomDisplayPhoto
     })
     // console.log(Math.floor(scroll/this.state.photoHeight))
   }
 
   getPhotoDimensions = (height, width) => {
     this.setState({
-      photoHeight: height,
+      photoHeight: 290,
       photoWidth: width
     })
   }
@@ -144,9 +147,12 @@ class ImageGallery extends Component {
         <Grid>
           <PhotoRows  photosNumbers={this.state.photosNumbers} 
                       elapsedScrollHeight={this.state.elapsedScrollHeight}
-                                          getPhotoDimensions={this.getPhotoDimensions}
-
-                      remainingScrollHeight={this.state.remainingScrollHeight}/>
+                      getPhotoDimensions={this.getPhotoDimensions}
+                      remainingScrollHeight={this.state.remainingScrollHeight}
+                      topDisplayRow={this.state.topDisplayRow}
+                      topVisibleRow={this.state.topVisibleRow}
+                      topDisplayPhoto={this.state.topDisplayPhoto}
+                      bottomDisplayPhoto={this.state.bottomDisplayPhoto}/>
         </Grid>
       </React.Fragment>
     );
@@ -156,42 +162,27 @@ class ImageGallery extends Component {
 export default ImageGallery;
 
 
-  // <Row style={{height: this.state.elapsedScrollHeight}}></Row>
-  // <Row>
-  //   {photos}
-  // </Row>
-  // <Row style={{height: (this.state.remainingScrollHeight)}}></Row>
-
-    //       <PhotoRows  getPhotoDimensions={this.getPhotoDimensions} 
-    //         firstVisiblePhoto={this.state.firstVisiblePhoto}
-    //         lastVisiblePhoto={this.state.lastVisiblePhoto}
-    //         firstDisplayPhoto={this.state.firstDisplayPhoto}
-    //         lastDisplayPhoto={this.state.lastDisplayPhoto}
-    //         photoHeight={this.state.photoHeight}
-    //         visibleStartRow={this.state.visibleStartRow}
-    //         visibleEndRow={this.state.visibleEndRow}
-    //         displayStartRow={this.state.displayStartRow}
-    //         displayEndRow={this.state.displayEndRow}
-    //         total={this.state.totalPhotos}
-    //         photosNumbers={this.state.photosNumbers}
-    //         />
-
-
 class PhotoRows extends Component {
 
   state={
     shouldUpdate: true,
     total: 0,
-    displayStart: 0,
-    displayEnd: 0
+    topDisplayRow: 0,
+    topVisibleRow: 0,
+    photos: getNumberArray(1500348260, 50)
   }
 
   componentWillReceiveProps(nextProps){
     let shouldUpdate = false;
-    if(this.props.photosNumbers !== nextProps.photosNumbers){
+    if(this.state.topDisplayRow <= nextProps.topDisplayRow || this.state.topVisibleRow >= nextProps.topVisibleRow){
       shouldUpdate = true
+      this.setState({
+        shouldUpdate: shouldUpdate,
+        topDisplayRow: nextProps.topDisplayRow,
+        topVisibleRow: nextProps.topVisibleRow, 
+        photos: getPhotoNumbersArr(nextProps.topDisplayPhoto, nextProps.bottomDisplayPhoto) 
+      })
     }
-    this.setState({shouldUpdate: shouldUpdate})
   }
 
   shouldComponentUpdate(){
@@ -201,7 +192,7 @@ class PhotoRows extends Component {
 
   render(){
     // console.log(this.props)
-    let photos = this.props.photosNumbers.map((number, i) => {
+    let photos = this.state.photos.map((number, i) => {
       return <Photo url={number} key={number+`${i}`} 
                     getPhotoDimensions={this.props.getPhotoDimensions}
                     photoHeight={this.props.photoHeight}/>
@@ -210,11 +201,13 @@ class PhotoRows extends Component {
 
     return(
       <React.Fragment>
-        <Row style={{height: this.props.elapsedScrollHeight}}></Row>
+        {/* <Row ><div style={{height: this.props.elapsedScrollHeight}}></div></Row> */}
+        <script style={{height: this.props.elapsedScrollHeight}}></script>
           <Row>
             {photos}
           </Row>
-        <Row style={{height: this.props.remainingScrollHeight}}></Row>
+          <script style={{height: this.props.remainingScrollHeight}}></script>
+        {/* <Row ><div style={{height: this.props.remainingScrollHeight}}></div></Row> */}
       </React.Fragment>
     )
   }
